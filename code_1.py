de92bc08dc1f0e93c6a548ae02e19c82fb40c7df
VAST Julien <jvast@douane.local>
2024-06-07 17:17:03 +0200

    test

--- code_1.py
+++ code_1.py
@@ -1,9 +1,11 @@
 
+from module import obtenir_temps
 from datetime import datetime
-print("Hello world ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
-
+print(obtenir_temps())
 print(5)
 print("Test1")
 print("Test2")
 
 print("Test3")
+
+print("Test 4")
