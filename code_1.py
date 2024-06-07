e4efa7b0a3aa9deb6201073cc4077f461bd178c1
VAST Julien <jvast@douane.local>
2024-06-07 13:39:01 +0200

    Test 2

--- code_1.py
+++ code_1.py
@@ -2,8 +2,4 @@
 from datetime import datetime
 print("Hello world ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
 
-<<<<<<< HEAD
 print(5)
-=======
-print(6)
->>>>>>> 61395e8b245df541d43d38e0713ea854cf734c62
